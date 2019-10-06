547e81367a1faec67366e9b57f5cf9c4bf4651b2
AzmanMD <md.azman138gmail.com>
2019-10-06 21:11:19 +0600

    eAppair

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.fa-bars').addEventListener('click', function() {
+  var element1 = document.querySelector('#particles-js');
+  element1.classList.toggle('toggleParticle');
+});
